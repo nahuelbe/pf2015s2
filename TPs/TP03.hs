e203b6595c34a72a66a0cfcaa319865e2e3245ba
nahuelbe <beniteznahueloscar@gmail.com>
2015-09-10 15:28:08 -0300

    agregados cambios

---
+++ TPs/TP03.hs
@@ -0,0 +1,46 @@
+--5) 
+
+--a)
+-- \f x-> f (f x)
+
+--b)
+-- \f x y -> f y x
+
+--C)
+-- \_ -> (+1)
+
+--6)
+
+--a)
+fix :: (a -> a) -> a
+fix f = f (fix f)
+
+--from :: Int -> [Int] DESAFIO
+
+summ :: [Int] -> Int 
+summ = fix (\r xs -> if  null xs then 0 else (head xs) + r (tail xs ))
+
+
+--b)
+fork :: (a -> b, a -> c) -> a -> (b,c) 
+fork (f,g) x = (f x, g x)
+
+--c)
+apply :: (a -> b) -> a -> b
+apply f = f
+
+
+apply2 :: (a -> b) -> a -> b
+apply2 = id 
+
+--d)
+
+curry :: ((a,b) -> c) -> a -> b -> c
+curry f x y = f (x,y)
+
+2=3
+
+nats0@(zero:nats1) = 0 : [1..]
+
+
+
